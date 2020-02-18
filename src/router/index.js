c19232410162a5f1ce72530dafd21645504f171b
Farizal Hamami <farizalhamami@gmail.com>
2020-02-18 14:17:00 +0700

    Create axios instances

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import test from '@/pages/Lorico/test'
 import TestPage from '@/pages/test/TestPage'
-import testlah from '@/pages/gold/testlah'
 
 Vue.use(Router)
 
@@ -17,18 +15,7 @@ export default new Router({
     },
     {
       path: '/test',
-      name: 'Page.Test',
       component: TestPage
-    },
-    {
-      path: '/lorico',
-      name: 'Page.Lorico',
-      component: test
-    },
-    {
-      path: '/job-search',
-      name: 'Page.Gold',
-      component: testlah
     }
   ]
 })
