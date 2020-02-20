6b19266f7c2b9455aaa1eda6bb745a4ea92773a0
Farizal Hamami <farizalhamami@gmail.com>
2020-02-20 12:42:22 +0700

    Create category list component

---
+++ src/components/List/CategoryList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="category-list">
+    <ul>
+      <li v-for="category in data" :key="category.Id">
+        {{ category.Name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
